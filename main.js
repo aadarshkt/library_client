eb11dac2749195b288138a208884ad607579d1c0
aadarshkt <aadarshkt1729@gmail.com>
2023-12-29 10:53:10 +0530

    delete book event listener added

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const fetchPromise = fetch("http://localhost:8080/api/books");
 const list_div = document.getElementsByClassName("list");
 
+//dialog return value
+//delete dialog button
+const delete_dialog = document.getElementById("delete_dialog");
+
+//no button on delete dialog button
+const no_delete_dialog = delete_dialog.querySelector("#no_delete_dialog");
+no_delete_dialog.addEventListener("click", () => delete_dialog.close("No"));
+
+//yes button
+const yes_delete_dialog = delete_dialog.querySelector("#yes_delete_dialog");
+yes_delete_dialog.addEventListener("click", () => delete_dialog.close("Yes"));
+
 fetchPromise
   .then((response) => response.json())
   .then((data) => appendChild(data))
@@ -9,37 +21,65 @@ fetchPromise
 function appendChild(data) {
   const len = data.length;
   for (let i = 0; i < len; i++) {
-    const list_row = document.createElement("div");
-    list_row.classList.add("list_child");
+    const list_cild = document.createElement("div");
+    list_cild.classList.add("list_child");
+    if (i % 2 == 0) {
+      list_cild.style.backgroundColor = "grey";
+      list_cild.style.color = "white";
+    }
+
+    const serial_no = document.createElement("p");
+    serial_no.style.width = "5%";
+    serial_no.textContent = `${i + 1}.`;
+    list_cild.appendChild(serial_no);
 
     const title = document.createElement("p");
     title.textContent = `${data[i].title}`;
-    list_row.appendChild(title);
+    list_cild.appendChild(title);
 
     const author = document.createElement("p");
     author.textContent = `${data[i].author}`;
-    list_row.appendChild(author);
+    list_cild.appendChild(author);
 
     const year_published = document.createElement("p");
     year_published.textContent = `${data[i].year_published}`;
-    list_row.appendChild(year_published);
+    list_cild.appendChild(year_published);
 
     const ISBN = document.createElement("p");
     ISBN.textContent = `${data[i].ISBN}`;
-    list_row.appendChild(ISBN);
+    list_cild.appendChild(ISBN);
 
     //update button
     const update_button = document.createElement("button");
     update_button.classList.add("update_btn");
     update_button.textContent = "Update";
-    list_row.appendChild(update_button);
+    list_cild.appendChild(update_button);
 
     //delete button
     const delete_button = document.createElement("button");
     delete_button.classList.add("delete_btn");
     delete_button.textContent = "Delete";
-    list_row.appendChild(delete_button);
+    list_cild.appendChild(delete_button);
 
-    list_div[0].appendChild(list_row);
+    //listen for delete events.
+    delete_button.addEventListener("click", () => {
+      delete_dialog.showModal();
+
+      const dialog_close_handler = () => {
+        if (delete_dialog.returnValue === "Yes") {
+          handle_delete_book(data[i].id);
+          delete_dialog.removeEventListener("close", dialog_close_handler);
+        }
+      };
+
+      //get the returnvalue.
+      delete_dialog.addEventListener("close", dialog_close_handler);
+    });
+
+    list_div[0].appendChild(list_cild);
   }
 }
+
+const handle_delete_book = (id) => {
+  console.log(id);
+};
